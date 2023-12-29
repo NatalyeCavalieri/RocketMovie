912543df14be9e2aca39adf81142418c13cf9c06
Natalye <cavalieridev@gmail.com>
2023-12-29 07:57:35 +0100

    init project / created first page/ style global / first components

---
+++ src/styles/theme.js
@@ -0,0 +1,20 @@
+export default {
+  COLORS: {
+    BLACK: "rgba(0, 0, 0, 1)",
+
+    BACKGROUND_900: "rgba(28, 27, 30, 1)",
+    BACKGROUND_800: "rgba(38, 37, 41, 1)",
+    BACKGROUND_700: "rgba(49, 46, 56, 1)",
+    BACKGROUND_600: "rgba(62, 59, 71, 1)",
+  
+  
+
+
+    WHITE: "#F4EDE8",
+    PINK: "rgba(255, 133, 155, 1)",
+
+    GRAY_100: "rgba(148, 143, 153, 1)",
+    GRAY_300: "rgba(153, 149, 145, 1)",
+
+  },
+}
