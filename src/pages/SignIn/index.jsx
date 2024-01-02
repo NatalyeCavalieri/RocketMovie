0cdc67f9f53eecc68e9b4ecd693e1c430e6a0171
Natalye <cavalieridev@gmail.com>
2024-01-02 08:57:42 +0100

    created page Signin and component input

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,9 +1,24 @@
-import { Container } from './styles'
+import { Container, Form, Background } from "./styles"
+import { Input } from "../../components/Input"
+import { Button } from "../../components/Button"
+import { ButtonText } from "../../components/ButtonText"
+import { MdOutlineEmail } from "react-icons/md"
+import { RiLockPasswordFill } from "react-icons/ri"
 
-export function SignIn(){
-  return(
-<Container>
-
-</Container>
+export function SignIn() {
+  return (
+    <Container>
+      <Form>
+      <h1>RocketMovies</h1>
+      <p>Aplicação para acompanhar tudo que assistir.</p>
+        <h3>Faça seu login</h3>
+        <Input type="text" placeholder="Email" icon={MdOutlineEmail} />
+        <Input type="password" placeholder="Senha" icon={RiLockPasswordFill} />
+        <Button type="button" title="Entrar" />
+      <ButtonText 
+      title="Criar conta"/>
+      </Form>
+      <Background/>
+    </Container>
   )
 }
--- src/pages/SignIn/styles.js
+++ src/pages/SignIn/styles.js
@@ -1,5 +1,42 @@
-import styled from "styled-components";
+import styled from "styled-components"
+import backgroundImg from "../../../public/image.svg"
 
 export const Container = styled.div`
+  height: 100vh;
+  display: flex;
+  align-items: stretch;
+`
 
+export const Form = styled.form`
+  padding: 0 136px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+
+  > h1 {
+    color: ${({ theme }) => theme.COLORS.PINK};
+    font-size: 48px;
+  }
+
+  > p {
+    font-size: 14px;
+    margin-bottom: 48px;
+  }
+
+  > h3 {
+    margin-bottom: 48px;
+  }
+
+
+  > button {
+    width: 340px;
+    margin-bottom: 42px;
+  }
+`
+export const Background = styled.div`
+  flex: 1;
+  background: url(${backgroundImg}) no-repeat center center;
+  background-size: cover;
 `
