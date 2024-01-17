70c17dd2079ba9eaafaaa9207f2b5529df29d070
Natalye <cavalieridev@gmail.com>
2024-01-17 09:05:03 +0100

    sharing users data authenticated

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -1,18 +1,37 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext } from "react"
+import { api } from "../services/api"
+import { useState } from "react"
 
 export const AuthContext = createContext({})
 
-function AuthProvider({children}){
-return (
-  <AuthContext.Provider value={{ name: "Natalye", email: "natalye@gmail.com" }}>
-    {children}
-  </AuthContext.Provider>
-)
+function AuthProvider({ children }) {
+  const [data, setData] = useState({})
+  async function signIn({ email, password }) {
+    
+    try {
+      const response = await api.post("/sessions", { email, password })
+      const { user, token } = response.data
+      api.defaults.headers.authorization = `Bearer ${token}`
+      setData({ user, token })
+    } catch (error) {
+      if (error.response) {
+        alert(error.response.data.message)
+      } else {
+        alert("Not is possible enter")
+      }
+    }
+  }
+
+  return (
+    <AuthContext.Provider value={{ signIn, user: data.user }}>
+      {children}
+    </AuthContext.Provider>
+  )
 }
 
-function useAuth(){
-   const context = useContext(AuthContext)
-   return context
+function useAuth() {
+  const context = useContext(AuthContext)
+  return context
 }
 
-export {AuthProvider, useAuth}
+export { AuthProvider, useAuth }
--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -5,19 +5,36 @@ import { Button } from "../../components/Button"
 import { MdOutlineEmail } from "react-icons/md"
 import { RiLockPasswordFill } from "react-icons/ri"
 import { useAuth } from "../../hooks/auth.jsx"
+import { useState } from "react"
 
 export function SignIn() {
-  const data = useAuth()
-  console.log("Meu contexto =>", data)
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const { signIn } = useAuth()
+
+  function handleSignIn(){
+    signIn({email, password})
+  }
+
   return (
     <Container>
       <Form>
         <h1>RocketMovies</h1>
         <p>Aplicação para acompanhar tudo que assistir.</p>
         <h3>Faça seu login</h3>
-        <Input type="text" placeholder="Email" icon={MdOutlineEmail} />
-        <Input type="password" placeholder="Senha" icon={RiLockPasswordFill} />
-        <Button type="button" title="Entrar" />
+        <Input
+          type="text"
+          placeholder="Email"
+          icon={MdOutlineEmail}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <Input
+          type="password"
+          placeholder="Senha"
+          icon={RiLockPasswordFill}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <Button type="button" title="Entrar" onClick={handleSignIn} />
         <Link to="/register">Criar conta</Link>
       </Form>
       <Background />
