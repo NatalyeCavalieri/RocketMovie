5345687cf1593fd6a33cd910ebba169c820f99a4
Natalye <cavalieridev@gmail.com>
2024-01-18 06:51:05 +0100

    created function handleupdate

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -31,6 +31,22 @@ function AuthProvider({ children }) {
 
     setData({})
   }
+
+  async function updateProfile({user}){
+    try{
+      await api.put("/users", user)
+      localStorage.setItem("@rocketmovies:user", JSON.stringify(user))
+      setData({user, token: data.token})
+      alert("Profile updated")
+    }catch(error){
+      if (error.response) {
+        alert(error.response.data.message)
+      } else {
+        alert("Not is possible update the profile")
+      }
+    }
+  }
+
   useEffect(()=>{
     const token = localStorage.getItem("@rocketmovies:token")
     const user = localStorage.getItem("@rocketmovies:user")
@@ -45,7 +61,9 @@ function AuthProvider({ children }) {
   },[])
 
   return (
-    <AuthContext.Provider value={{ signIn, signOut, user: data.user }}>
+    <AuthContext.Provider
+      value={{ signIn, signOut, updateProfile, user: data.user }}
+    >
       {children}
     </AuthContext.Provider>
   )
--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -10,11 +10,22 @@ import {useAuth} from '../../hooks/auth'
 
 
 export function Profile() {
-  const {user} = useAuth()
+  const { user, updateProfile } = useAuth()
   const [name, setName] = useState(user.name)
   const [email, setEmail] = useState(user.email)
   const [passwordOld, setPasswordOld] = useState()
   const [passwordNew, setPasswordNew] = useState()
+
+  async function handleUpdate(){
+    const user = {
+      name,
+      email,
+      password: passwordNew,
+      old_password: passwordOld
+    }
+
+  await updateProfile({ user })
+  }
 
   return (
     <Container>
@@ -62,7 +73,7 @@ export function Profile() {
           icon={RiLockPasswordLine}
           onChange={(e) => setPasswordNew(e.target.value)}
         />
-        <Button type="button" title="Salvar" />
+        <Button type="button" title="Salvar" onClick={handleUpdate} />
       </Form>
     </Container>
   )
